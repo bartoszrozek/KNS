19bc287352c218ba62735f45d1baea1e12aea584
bartosz.rozek.stud@pw.edu.pl <Swave533>
2022-05-31 21:15:17 +0200

    second level

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
-from datetime import datetime
-
-from numpy import diff
+from flask import Flask, render_template, request, redirect, flash
 from logic.word import word
-from random import randrange
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'E1E2E5EF7E626E1B2E6FD98ED175C94A30377D3F50CAD28D591F7CA38C2BFAD6'
 
-currentWord = word(9,1)
+currentWord = word(9, 1)
 difficulty_level = 0
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -23,16 +20,17 @@ def index():
         if ncharAlphabet == "":
             flash('Nie podano długości alfabetu', category='error')
             return render_template('index.html')
-        elif int(ncharAlphabet) <1:
+        elif int(ncharAlphabet) < 1:
             flash('Zła długość alfabetu', category='error')
             return render_template('index.html')
         elif endPoint == "":
             flash('Nie podano długości gry', category='error')
             return render_template('index.html')
-        
+
         currentWord = word(int(ncharAlphabet), int(endPoint))
         difficulty_level = request.form['difficulty']
         return redirect('/game')
+
 
 @app.route('/game', methods=['POST', 'GET'])
 def game():
@@ -40,9 +38,9 @@ def game():
     indexesPre = currentWord.check_repetition()[1:]
     end = currentWord.length >= currentWord.endPoint
     if indexesPre:
-        indexes = list(range(indexesPre[0]-indexesPre[1]+1,indexesPre[0]+2))
-        indexes2 = list(range(indexesPre[0]+2,indexesPre[0]+indexesPre[1]+3))
-    else: 
+        indexes = list(range(indexesPre[0] - indexesPre[1] + 1, indexesPre[0] + 2))
+        indexes2 = list(range(indexesPre[0] + 2, indexesPre[0] + indexesPre[1] + 3))
+    else:
         indexes = None
         indexes2 = None
     letters = currentWord.letters
@@ -51,7 +49,8 @@ def game():
         flash('Zwycięstwo!', category='information')
     elif end:
         flash('Osiągnięto długość krytyczną. Przegrana!', category='error')
-    return render_template('game.html', letters = letters, indexes = indexes, indexes2 = indexes2, end = end)
+    return render_template('game.html', letters=letters, indexes=indexes, indexes2=indexes2, end=end)
+
 
 @app.route('/add/<int:id>')
 def add(id):
@@ -63,14 +62,16 @@ def add(id):
         if difficulty_level == "Easy":
             currentWord.computer_move(id)
         else:
-            #change method below for harder mode
-            currentWord.computer_move(id)
+            # change method below for harder mode
+            currentWord.ext_computer_move(id)
         return redirect('/game')
+
 
 @app.route('/reset')
 def reset():
     global currentWord
     return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- logic/word.py
+++ logic/word.py
@@ -1,47 +1,76 @@
-from random import randrange
 import random
 import copy
 
+
 class word:
-    
+
     def __init__(self, alphabet_length, endPoint):
         alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
-        
+
         self.letters = []
         self.length = 0
         self.alphabet = alphabet[0:alphabet_length]
         self.endPoint = endPoint
-        
+
     def show(self):
         print(("".join(["{" + str(i) + ":^5}" for i in range(self.length)])).format(*self.letters))
-        print(("".join(["{" + str(i) + ":<5}" for i in range(self.length + 1)])).format(*list(range(0,self.length+1))))
-        
+        print(
+            ("".join(["{" + str(i) + ":<5}" for i in range(self.length + 1)])).format(*list(range(0, self.length + 1))))
+
     def add_letter(self, letter, place):
         try:
-            if(letter not in self.alphabet):
+            if (letter not in self.alphabet):
                 raise ValueError('Letter is not from alphabet')
             self.letters.insert(place, letter)
             self.length += 1
         except ValueError as err:
             print(err)
-            
+
     def check_repetition(self):
-        for i in range(self.length-1):
-            for j in range(min(i + 1, self.length-i-1)):
-                if(self.letters[i-j:i+1] == self.letters[i+1:i+j+2]):
+        for i in range(self.length - 1):
+            for j in range(min(i + 1, self.length - i - 1)):
+                if (self.letters[i - j:i + 1] == self.letters[i + 1:i + j + 2]):
                     return [True, i, j]
-        else: 
-            return [False]
-        
+        return [False]
+
     def computer_move(self, place):
         letters = list(self.alphabet)
         random.shuffle(letters)
         for letter in letters:
             temp = copy.deepcopy(self)
             temp.add_letter(letter, place)
-            if(not temp.check_repetition()[0]):
+            if (not temp.check_repetition()[0]):
                 self.letters = temp.letters
                 self.length = temp.length
                 return
+        self.letters = temp.letters
+        self.length = temp.length
+
+    def loosing_position(self, place):
+        letters = list(self.alphabet)
+        random.shuffle(letters)
+        for letter in letters:
+            temp = copy.deepcopy(self)
+            temp.add_letter(letter, place)
+            if not temp.check_repetition()[0]:
+                return False
+        return True
+
+    def ext_computer_move(self, place):
+        letters = list(self.alphabet)
+        random.shuffle(letters)
+        for letter in letters:
+            temp = copy.deepcopy(self)
+            temp.add_letter(letter, place)
+            if (not temp.check_repetition()[0]):
+                computer_is_loosing = False
+                for human_choice in range(temp.length + 1):
+                    if temp.loosing_position(human_choice):
+                        computer_is_loosing = True
+                        break
+                if not computer_is_loosing:
+                    self.letters = temp.letters
+                    self.length = temp.length
+                    return
         self.letters = temp.letters
         self.length = temp.length
